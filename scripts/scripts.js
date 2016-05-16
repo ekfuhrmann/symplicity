e348bcd4551782ad74d3b9f3b2284b875f5d122b
Eric Fuhrmann <ekfuhrmann@gmail.com>
2016-05-16 03:17:30 -0400

    Added the Hero content to the site and renamed the project from 'Symplicit' to 'Symplicity'

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,47 +1,32 @@
-function showPassword() {
-   var passwordInput = document.getElementById('password');
-   passwordInput.type = passwordInput.type === 'text' ? 'password' : 'text';
-}
+var animateHeader = (function() {
+  var docElem = document.documentElement,
+    header = document.querySelector( '.header' ),
+    didScroll = false,
+    changeHeaderOn = 190;
 
-// Alerts Controls
-function showAlert(message) {
-    message = message || 'This is a test message.';
-    document.getElementById('alert').innerHTML = message;
-    document.getElementById('alert').className = 'main__alert--validation';
-};
+  function init() {
+    window.addEventListener( 'scroll', function () {
+     if ( !didScroll ) {
+       didScroll = true;
+       setTimeout( scrollPage, 250 );
+     }
+    }, false );
+  }
 
-function clearAlert() {
-    document.getElementById('alert').className = 'main__alert';
-};
-
-function checkForm(event) {
-    var username = document.getElementById('username');
-    var password = document.getElementById('password');
-
-    if (username.value == '' && password.value == '') {
-        event.preventDefault();
-        showAlert('The username and password fields can not be blank.');
-        username.className = 'main__form_input--validation';
-        password.className = 'main__form_input--validation';
-    } else if (username.value == '') {
-        event.preventDefault();
-        showAlert('The username field can not be blank.');
-        username.className = 'main__form_input--validation';
-        password.className = 'main__form_input';
-    } else if (password.value == '') {
-        event.preventDefault();
-        showAlert('The password field can not be blank.');
-        password.className = 'main__form_input--validation';
-        username.className = 'main__form_input';
-    } else if (password.value.match(/^[0-9a-zA-Z]+$/) || password.value.length < 6) {
-        event.preventDefault();
-        showAlert('The password field must be at least 6 characters long and contain at least one capital letter and at least one non-alphanumeric character.');
-        password.className = 'main__form_input--validation';
-        username.className = 'main__form_input';
+  function scrollPage() {
+    var sy = scrollY();
+    if ( sy >= changeHeaderOn ) {
+      header.classList.add( 'header--shrink' );
     } else {
-        event.preventDefault();
-        password.className = 'main__form_input';
-        username.className = 'main__form_input';
-        document.getElementById('alert').className = 'main__alert';
+      header.classList.remove( 'header--shrink' );
     }
-};
+    didScroll = false;
+  }
+
+  function scrollY() {
+    return window.pageYOffset || docElem.scrollTop;
+  }
+
+  init();
+
+})();
